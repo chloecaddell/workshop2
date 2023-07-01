a8305965bd455521b556b8cc5d332191c241f955
Chloe Caddell <chloecaddell3@gmail.com>
2023-07-01 12:59:21 -0500

    update stuff

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['976114823730.dkr.ecr.us-east-2.amazonaws.com/workshop2', '0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ec2-3-141-80-246.us-east-2.compute.amazonaws.com',
+                 '0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
